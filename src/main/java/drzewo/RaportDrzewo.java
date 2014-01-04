02b9887fc3347c0a67ecd6f581eb318c98e211f0
magiclud <you@example.com>
2014-01-04 20:17:34 +0100

    raport drzewo 3

--- src/main/java/drzewo/RaportDrzewo.java
+++ src/main/java/drzewo/RaportDrzewo.java
@@ -1,10 +1,36 @@
 package drzewo;
 
-public class RaportDrzewo {
+public class RaportDrzewo implements DrzewoBuilder {
 
+	Drzewo drzewo;
+	int wiek;
+	int wysokosc;
+	String nazwa;
 
 	RaportDrzewo(Drzewo drzewo, String nazwa) {
+		this.drzewo = drzewo;
+		wiek = drzewo.getWiek();
+		wysokosc = drzewo.getWysokosc();
+		this.nazwa = nazwa;
 		System.out.println(nazwa + " wiek " + drzewo.getWiek() + " wysokosc " + drzewo.getWysokosc());
+	}
+
+	@Override
+	public void buildWiek() {
+		drzewo.setWiek(wiek);
+
+	}
+
+	@Override
+	public void buildWysokosc() {
+		drzewo.setWysokosc(wysokosc);
+
+	}
+
+	@Override
+	public Drzewo getDrzewo() {
+		// TODO Auto-generated method stub
+		return drzewo;
 	}
 
 }
