dc4b681a241e039d4be1d82ce25d260347e4bac8
magiclud <you@example.com>
2014-01-06 09:34:13 +0100

    builder problemy2

---
+++ src/main/java/eu/jpereira/trainings/designpatterns/creational/builder/json/JSONReportBudowniczy.java
@@ -0,0 +1,10 @@
+package eu.jpereira.trainings.designpatterns.creational.builder.json;
+
+import eu.jpereira.trainings.designpatterns.creational.builder.model.ReportBody;
+
+public interface JSONReportBudowniczy {
+
+	public void addContent();
+
+	ReportBody getReport();
+}
